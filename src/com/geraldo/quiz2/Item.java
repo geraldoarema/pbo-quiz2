c2794d191de9b818fcce0ceb3b25e6b9d90b82b3
Geraldo B. I <geraldoarema87@gmail.com>
2018-11-30 21:47:51 +0700

    Membuat class Item
    Membuat variable , constraktor , setter , getter , toString

---
+++ src/com/geraldo/quiz2/Item.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.geraldo.quiz2;
+
+/**
+ *
+ * @author ALDO
+ */
+public class Item {
+    
+    private String nama;
+    private float harga;
+    private int jumlah;
+
+    public Item() {
+    }
+
+    public Item(String nama, float harga, int jumlah) {
+        this.nama = nama;
+        this.harga = harga;
+        this.jumlah = jumlah;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public float getHarga() {
+        return harga;
+    }
+
+    public void setHarga(float harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    @Override
+    public String toString() {
+        return  this.nama;
+    }
+    
+    
+    
+    
+}
